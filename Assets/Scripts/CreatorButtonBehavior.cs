9406d73e9c353d9a46a2c2048110c3b41cb3ea74
Nick Richter <nichter11@gmail.com>
2020-02-12 12:50:31 -0500

    Basic UI and first upload

---
+++ Assets/Scripts/CreatorButtonBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreatorButtonBehavior : MonoBehaviour
+{
+    public Button button;
+    public Text nameText;
+    public Image iconImage;
+
+    private CreatorItem item;
+    private CreatorContentBehavior creatorContent;
+
+    void Start()
+    {
+        button.onClick.AddListener(HandleClick);
+    }
+
+    public void Setup(CreatorItem currentItem, CreatorContentBehavior currentContent)
+    {
+        item = currentItem;
+        nameText.text = item.name;
+        iconImage.sprite = item.icon;
+        creatorContent = currentContent;
+    }
+
+    public void HandleClick()
+    {
+        // whatever clicking this button should do
+        Debug.Log("Button Clicked");
+    }
+
+    
+}
---
+++ Assets/Scripts/CreatorContentBehavior.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class CreatorItem
+{
+    public string name;
+    public Sprite icon;
+}
+public class CreatorContentBehavior : MonoBehaviour
+{
+    public List<CreatorItem> itemList;
+    public Transform contentPanel;
+    public ObjectPool objectPool;
+
+    void Start()
+    {
+        // RemoveButtons();
+        AddButtons();
+    }
+
+    private void AddButtons()
+    {
+        foreach (CreatorItem item in itemList)
+        {
+            GameObject newButton = objectPool.GetObject();
+            newButton.transform.SetParent(contentPanel, false);
+
+            CreatorButtonBehavior creatorButton = newButton.GetComponent<CreatorButtonBehavior>();
+            creatorButton.Setup(item, this);
+        }
+    }
+
+    private void RemoveButtons()
+    {
+        while (contentPanel.childCount > 0)
+        {
+            GameObject removing = transform.GetChild(0).gameObject;
+            objectPool.ReturnToInactivePool(removing);
+        }
+    }
+
+    private void AddItem(CreatorItem item)
+    {
+        itemList.Add(item);
+    }
+
+    private void RemoveItem(CreatorItem item)
+    {
+        itemList.Remove(item);
+    }
+}
