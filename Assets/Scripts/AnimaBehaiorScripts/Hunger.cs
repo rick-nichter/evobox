4f398c314f961ba2b58197883fab4378b3e9461d
eiden.m <eiden.m@husky.neu.edu>
2020-04-01 21:49:01 -0400

    initial hunger implementation

---
+++ Assets/Scripts/AnimaBehaiorScripts/Hunger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hunger : MonoBehaviour
+{
+
+    public int hungerPoints = 10; 
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (hungerPoints > 0)
+        {
+            Invoke(nameof(loseHungerPoint), 10);
+        }
+        else
+        {
+            Die();
+        }
+    }
+
+    // 1 in 10 chance to lose a hunger point.  This function should be called on delay
+    void loseHungerPoint()
+    {
+        if (Random.Range(0, 10) == 5)
+        {
+            hungerPoints--;
+        }
+    }
+
+    public void Eat()
+    {
+        hungerPoints++;
+    }
+
+    void Die()
+    {
+        //TODO Notify user of death in some way
+        Destroy(gameObject);
+    }
+}
