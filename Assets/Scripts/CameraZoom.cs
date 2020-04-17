b661df9821612bb8bb905db1acafbc2e35ef140d
eiden.m <eiden.m@husky.neu.edu>
2020-04-17 17:58:04 -0400

    add camera zoom to fulfill player reqs

---
+++ Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    public float minFOV = 30f;
+    public float maxFOV = 80f;
+    public float sensitivity = 10f;
+    public Camera mainCamera; 
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float FOV = mainCamera.fieldOfView;
+        FOV += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+        FOV = Mathf.Clamp(FOV, minFOV, maxFOV);
+        mainCamera.fieldOfView = FOV; 
+    }
+}
