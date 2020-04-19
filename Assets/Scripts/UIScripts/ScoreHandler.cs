cb902ca0b89a313979d6e2d250df484b7264215a
Nick Richter <nichter11@gmail.com>
2020-04-18 21:20:50 -0400

    Added high score functionality

--- Assets/Scripts/UIScripts/ScoreHandler.cs
+++ Assets/Scripts/UIScripts/ScoreHandler.cs
@@ -8,9 +8,11 @@ public class ScoreHandler : MonoBehaviour
 {
     private int score;
     public Text scoreTextObject;
+    public Text highScoreText;
     public Hashtable animalScoreValues = new Hashtable();
     void Start()
     {
+        highScoreText.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
         score = 0;
         scoreTextObject.text = score.ToString();
         animalScoreValues.Add("Deer", 3);
@@ -23,15 +25,20 @@ public class ScoreHandler : MonoBehaviour
 
     public void changeScore(int deltaScore)
     {
-      score += deltaScore;
+        score += deltaScore;
       
-      //don't allow negative score after score decrements on animal death
-      if (score < 0)
-      {
-          score = 0;
-      }
-      
-      scoreTextObject.text = score.ToString();
+        //don't allow negative score after score decrements on animal death
+        if (score < 0)
+        {
+            score = 0;
+        }
+
+        if (score > PlayerPrefs.GetInt("HighScore", 0))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+        
+        scoreTextObject.text = score.ToString();
     }
 
     private void UpdateScore()
