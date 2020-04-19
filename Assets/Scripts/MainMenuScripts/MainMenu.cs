98dfe37f1f4518bee072ebd676e3e457d5f8ed9b
eiden.m <eiden.m@husky.neu.edu>
2020-04-19 19:34:29 -0400

    finished main menu

--- Assets/Scripts/MainMenuScripts/MainMenu.cs
+++ Assets/Scripts/MainMenuScripts/MainMenu.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public TextMeshProUGUI highScoreText; 
+    
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void GetHighScore()
+    {
+        highScoreText.text = "High Score -> " + PlayerPrefs.GetInt("HighScore", 0).ToString(); 
     }
 
 }
