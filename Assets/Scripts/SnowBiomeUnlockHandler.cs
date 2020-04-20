255e90c15f4551e95e01af7b62edeb330fa9bf90
eiden.m <eiden.m@husky.neu.edu>
2020-04-19 21:10:08 -0400

    add jons mountain biome

--- Assets/Scripts/SnowBiomeUnlockHandler.cs
+++ Assets/Scripts/SnowBiomeUnlockHandler.cs
@@ -5,59 +5,122 @@ using UnityEngine.UI;
 
 public class SnowBiomeUnlockHandler : MonoBehaviour
 {
-    public GameObject info;
-    public GameObject Unlock;
-    public GameObject snowBiomeArrow;
+    public GameObject info_snow;
+    public GameObject Unlock_snow;
+    public GameObject info_mountain;
+    public GameObject Unlock_mountain;
+    public GameObject snowBiomeArrow_fromMountain;
+    public GameObject snowBiomeArrow_fromMain;
     public GameObject mainBiomeArrow;
+    public GameObject mountainBiomeArrow;
     public Transform mainCameraPos;
-    public Transform snowCameraPos; 
+    public Transform snowCameraPos;
+    public Transform mountainCameraPos;
     public GameObject snowBiomeDividers;
-    
-    private bool snowUnlocked = false; 
+    public GameObject mountainBiomeDividers;
+
+    public int currentBiome = 1;
+
+
+    private bool snowUnlocked = false;
+    private bool mountainUnlocked = false;
+
 
     // Update is called once per frame
     void Update()
     {
         if (!snowUnlocked)
         {
-            if (GameObject.FindGameObjectsWithTag("Wolf").Length >= 1
-                && GameObject.FindGameObjectsWithTag("Deer").Length >= 1)
+            if (GameObject.FindGameObjectsWithTag("Wolf").Length >= 5
+                && GameObject.FindGameObjectsWithTag("Deer").Length >= 5
+                && GameObject.FindGameObjectsWithTag("Bear").Length >= 3
+                && currentBiome == 1)
             {
-                info.SetActive(false);
-                Unlock.SetActive(true);
+                info_snow.SetActive(false);
+                Unlock_snow.SetActive(true);
             }
             else
             {
-                info.SetActive(true);
-                Unlock.SetActive(false);
+                info_snow.SetActive(true);
+                Unlock_snow.SetActive(false);
+            }
+        }
+        if (!mountainUnlocked)
+        {
+            if (GameObject.FindGameObjectsWithTag("Bear").Length >= 10 
+                && currentBiome == 2)
+            {
+                info_mountain.SetActive(false);
+                Unlock_mountain.SetActive(true);
+            }
+            else
+            {
+                info_mountain.SetActive(true);
+                Unlock_mountain.SetActive(false);
             }
         }
     }
 
-    public void onUnlockClick()
+    public void onUnlockSnowClick()
     {
         snowUnlocked = true; 
         snowBiomeDividers.SetActive(false);
-        Unlock.SetActive(false);
-        snowBiomeArrow.SetActive(true);
+        Unlock_snow.SetActive(false);
+        snowBiomeArrow_fromMain.SetActive(true);
     }
 
-    public void onSnowBiomeArrowClick()
+    public void onUnlockMountainClick()
     {
-        snowBiomeArrow.SetActive(false);
-        mainBiomeArrow.SetActive(true);
-        // TODO make this lerp nicely
-        Camera.main.transform.position = snowCameraPos.position;
-        Camera.main.transform.rotation = snowCameraPos.rotation;
-        Camera.main.GetComponent<CameraZoom>().UpdateCameraStartPosition(snowCameraPos.position);
+        mountainUnlocked = true; 
+        mountainBiomeDividers.SetActive(false);
+        Unlock_mountain.SetActive(false);
+        mountainBiomeArrow.SetActive(true);
     }
 
     public void onMainBiomeArrowClick()
     {
-        snowBiomeArrow.SetActive(true);
+        currentBiome = 1;
+        snowBiomeArrow_fromMain.SetActive(true);
         mainBiomeArrow.SetActive(false);
+        mountainBiomeArrow.SetActive(false);
         Camera.main.transform.position = mainCameraPos.position;
         Camera.main.transform.rotation = mainCameraPos.rotation;
-        Camera.main.GetComponent<CameraZoom>().UpdateCameraStartPosition(mainCameraPos.position);
+    }
+
+    public void onSnowBiomeArrowClick_FromMain()
+    {
+        currentBiome = 2;
+        snowBiomeArrow_fromMain.SetActive(false);
+        if (mountainUnlocked)
+        {
+            mountainBiomeArrow.SetActive(true);
+        }
+        mainBiomeArrow.SetActive(true);
+        // TODO make this lerp nicely
+        Camera.main.transform.position = snowCameraPos.position;
+        Camera.main.transform.rotation = snowCameraPos.rotation; 
+    }
+
+    public void onSnowBiomeArrowClick_fromMountain()
+    {
+        currentBiome = 2;
+        snowBiomeArrow_fromMountain.SetActive(false);
+        mountainBiomeArrow.SetActive(true);
+        mainBiomeArrow.SetActive(true);
+        // TODO make this lerp nicely
+        Camera.main.transform.position = snowCameraPos.position;
+        Camera.main.transform.rotation = snowCameraPos.rotation;
+    }
+
+
+
+    public void onMountainBiomeArrowClick()
+    {
+        currentBiome = 3;
+        snowBiomeArrow_fromMountain.SetActive(true);
+        mainBiomeArrow.SetActive(false);
+        mountainBiomeArrow.SetActive(false);
+        Camera.main.transform.position = mountainCameraPos.position;
+        Camera.main.transform.rotation = mountainCameraPos.rotation;
     }
 }
